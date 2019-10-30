9f8f0ca3b859a1609a791666843485ec0a247f60
William Sedlacek <wsedlacekc@gmail.com>
2019-10-30 13:17:17 -0500

    Setup /api/post endpoint.
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,11 +1,32 @@
 import express from 'express';
 
-import { validatePostID } from '../middleware';
+import { validatePost, validatePostID } from '../middleware';
+import { get, update, remove } from '../data/post';
+
 export const postRouter = express.Router();
 
-postRouter.get('/', (req, res) => {});
+postRouter.get('/', async (req, res) => res.json(await get()));
 
+postRouter.use('/:id', validatePost());
 postRouter.use('/:id', validatePostID());
-postRouter.get('/:id', (req, res) => {});
-postRouter.put('/:id', (req, res) => {});
-postRouter.delete('/:id', (req, res) => {});
+postRouter.get('/:id', async (req, res) => res.json(req.post));
+
+postRouter.put('/:id', async (req, res) => {
+  try {
+    const post = await update(req.post.id, req.body);
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Could not update post' });
+  }
+});
+
+postRouter.delete('/:id', async (req, res) => {
+  try {
+    const post = await remove(req.post.id);
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Could not delete post' });
+  }
+});
