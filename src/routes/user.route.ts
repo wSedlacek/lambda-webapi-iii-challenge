5ce5d988938191d25a99fd9d25029a394f659290
William Sedlacek <wsedlacekc@gmail.com>
2019-10-30 13:17:26 -0500

    Setup /api/user endpoint
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,17 +1,52 @@
 import express from 'express';
 import { validateUser, validateUserID, validatePost } from '../middleware';
+import { get, insert, update, remove } from '../data/user';
+import { insert as insertPost } from '../data/post';
 
 export const userRouter = express.Router();
 
 userRouter.use('/', validateUser());
-userRouter.get('/', (req, res) => {});
-userRouter.post('/', (req, res) => {});
+userRouter.get('/', async (req, res) => res.json(await get()));
+userRouter.post('/', async (req, res) => {
+  try {
+    const user = await insert(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Could not create user' });
+  }
+});
 
 userRouter.use('/:id', validateUserID());
-userRouter.get('/:id', (req, res) => {});
-userRouter.put('/:id', (req, res) => {});
-userRouter.delete('/:id', (req, res) => {});
+userRouter.get('/:id', async (req, res) => res.json(req.user));
+userRouter.put('/:id', async (req, res) => {
+  try {
+    const user = await update(req.user.id, req.body);
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Could not update user' });
+  }
+});
+
+userRouter.delete('/:id', async (req, res) => {
+  try {
+    const user = await remove(req.user.id);
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Could not delete user' });
+  }
+});
 
 userRouter.use('/:id/post', validatePost());
-userRouter.get('/:id/posts', (req, res) => {});
-userRouter.post('/:id/posts', (req, res) => {});
+userRouter.get('/:id/posts', async (req, res) => res.json(req.posts));
+userRouter.post('/:id/posts', async (req, res) => {
+  try {
+    const post = await insertPost({ ...req.body, user_id: req.user.id });
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Could not create post' });
+  }
+});
