2eb9b1bb68f0b5ad39d543c6461eea184e9af68a
William Sedlacek <wsedlacekc@gmail.com>
2019-10-30 12:41:59 -0500

    Fix routes
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import { Middleware } from './models';
 import { postRouter, userRouter } from './routes';
 
 export const server = express();
-export const logger: Middleware = () => (req, res, next) => {
+const logger: Middleware = () => (req, res, next) => {
   console.log(`\n${new Date().toISOString()} ${req.method} ${req.url}\n`);
   next();
 };
 
+server.use(express.json());
 server.use(logger());
 server.get('/', (req, res) => res.send(`<h2>Let's write some middleware!</h2>`));
-server.use(postRouter);
-server.use(userRouter);
+server.use('/api/post', postRouter);
+server.use('/api/user', userRouter);
